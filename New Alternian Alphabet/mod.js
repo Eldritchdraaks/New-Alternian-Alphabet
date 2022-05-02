b5c24d812e8418a2184d38ad94139d4783efaf28
Eldritchdraaks <101802705+Eldritchdraaks@users.noreply.github.com>
2022-05-02 03:43:24 -0500

    Add files via upload

--- New Alternian Alphabet/mod.js
+++ New Alternian Alphabet/mod.js
@@ -2,8 +2,8 @@ module.exports = {
     title: "New Alternian Alphabet", 
     summary: "Replaces the Daedric alphabet with the one from Hiveswap",
     author: "Eldritchdraaks",
-    modVersion: 1.0,
-    description: "The Alternian alphabet used in the comic is a modified version of Daedric alphabet from <i>The Elder Scrolls</i>. When <i>Hiveswap</i> was in production, the team made a new original alphabet that became the new canon for Alternia. This mod replaces the old alphabet with the new. Additionally, it now reads left to right, which is also now canon.",
+    modVersion: 1.1,
+    description: "The Alternian alphabet used in the comic is a modified version of Daedric alphabet from <i>The Elder Scrolls</i>. When <i>Hiveswap</i> was in production, the team made a new original alphabet that became the new canon for Alternia. This mod replaces the old alphabet with the new. Additionally, it now reads left to right, which is also now canon.<br><br>'Hiveswap Alternian' font by OrangeyPeels.",
 
 	trees: {
 		"./content/": "assets://storyfiles/hs2/",
